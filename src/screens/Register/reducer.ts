da12bd986100b4ba5f6c8b8c7e875576b59adbea
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-16 18:09:33 +0100

    feat: create register reducer

---
+++ src/screens/Register/reducer.ts
@@ -0,0 +1,63 @@
+import {
+    REGISTER_SET_EMAIL,
+    REGISTER_SET_PASSWORD,
+    REGISTER_SET_PHONE_NUMBER,
+    REGISTER_SET_FIRSTNAME,
+    REGISTER_SET_LASTNAME,
+    REGISTER_SET_ADRESS,
+    REGISTER_SET_CITY,
+    REGISTER_SET_ZIP_CODE,
+    REGISTER_SET_STATUS,
+    REGISTER_SET_ACTIVITY_AREA,
+} from './action';
+
+
+const initialState = {
+    email: '',
+    password: '',
+    phoneNumber: '',
+    firstname: '',
+    lastname:'',
+    adress: '',
+    city: '',
+    zipCode: '',
+    status: '',
+    activityArea: '',
+};
+
+export default function(state = initialState, action: any) {
+    switch(action.type) {
+        case REGISTER_SET_ADRESS:
+            return {...state, email: action.value};
+
+        case REGISTER_SET_PASSWORD:
+            return {...state, password: action.value};
+
+        case REGISTER_SET_PHONE_NUMBER:
+            return {...state, phoneNumber: action.value};
+
+        case REGISTER_SET_FIRSTNAME:
+            return {...state, firstname: action.value};
+
+        case REGISTER_SET_LASTNAME:
+            return {...state, lastname: action.value};
+
+        case REGISTER_SET_ADRESS:
+            return {...state, adress: action.value};
+
+        case REGISTER_SET_CITY:
+            return {...state, city: action.value};
+
+        case REGISTER_SET_ZIP_CODE:
+            return {...state, zipCode: action.value};
+
+        case REGISTER_SET_STATUS:
+            return {...state, status: action.value};
+
+        case REGISTER_SET_ACTIVITY_AREA:
+            return {...state, activityArea: action.value};
+
+        default:
+            return state;
+    }
+}
