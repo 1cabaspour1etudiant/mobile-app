e52f47fed8a0933fafa5cf77bbc915a40fe75f31
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-16 10:08:02 +0100

    feat: create utils's function checkError

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,1 +1,7 @@
 export const jsonMapper = (res: Response) => res.json();
+export const checkError = (res: Response) => {
+    if (res.status !== 200 && res.status !== 201) {
+        throw res.status;
+    }
+    return res;
+};
