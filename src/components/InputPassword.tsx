bf785579c2d524fa99b86446e12126fc5d41a26e
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-16 10:07:04 +0100

    refacto: remove dead code

--- src/components/InputPassword.tsx
+++ src/components/InputPassword.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
 import { Icon, Input } from '@ui-kitten/components';
 
-const AlertIcon = (props:any) => (
-  <Icon {...props} name='alert-circle-outline'/>
-);
-
 export default function InputPassword ({ value = '', onChanchValue = (v:string) => {} }) {
     const [secureTextEntry, setSecureTextEntry] = React.useState(true);
 
