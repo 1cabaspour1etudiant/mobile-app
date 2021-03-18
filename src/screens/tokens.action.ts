3c9ea2fdceee3360dfa97c1f7d8ab1bc036ea3bc
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-18 15:13:38 +0100

    fix: remove refreshToken

--- src/screens/tokens.action.ts
+++ src/screens/tokens.action.ts
@@ -1,11 +1,10 @@
 export const TOKENS_SET_TOKENS = 'TOKENS_SET_TOKENS';
 export const TOKENS_CLEAR_TOKENS = 'TOKENS_CLEAR_TOKENS';
 
-export const actionSetTokens = (accessToken: string, accessTokenExpirationDate: string, refreshToken: string) => ({
+export const actionSetTokens = (accessToken: string, accessTokenExpirationDate: string) => ({
   type: TOKENS_SET_TOKENS,
   value: {
     accessToken,
     accessTokenExpirationDate,
-    refreshToken,
   },
 });
