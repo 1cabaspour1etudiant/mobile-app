0c5666b7c742314e78f747e059c23095a3031492
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-22 21:22:15 +0100

    feat: implement searchTab v1

--- src/screens/Private/SearchTab.tsx
+++ src/screens/Private/SearchTab.tsx
@@ -1,11 +1,91 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Layout } from '@ui-kitten/components';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Icon, Layout, List, ListItem } from '@ui-kitten/components';
+import { getUserSearch } from '../../api/User';
+import { UserSearch } from '../../api/types';
+import SpinnerList from '../../components/SpinnerList';
+
+function UserListItem({ firstname, activityArea }: UserSearch) {
+    const renderItemAccessoryRight = useCallback(() => {
+        return (
+            <Button size='tiny'>Contacte</Button>
+        );
+    }, []);
+
+    const renderItemAccessoryLeft = useCallback((props) => {
+        return (
+            <Icon {...props} name='person'/>
+        );
+    }, []);
+
+    return (
+        <ListItem
+            title={firstname}
+            description={activityArea}
+            accessoryLeft={renderItemAccessoryLeft}
+            accessoryRight={renderItemAccessoryRight}
+        />
+    );
+}
 
 export default function SearchTab() {
+    const [loaded, setLoaded] = useState(false);
+    const [users, setUsers] = useState<UserSearch[]>([]);
+    const [page, setPage] = useState(-1);
+    const [lastPage, setLastPage] = useState(false);
+
+    useEffect(() => {
+        if (!loaded) {
+            let mounted = true;
+            const abortController = new AbortController();
+
+            getUserSearch(page + 1, 20, abortController)
+                .then(({ page, items, lastPage }) => {
+                    if (mounted) {
+                        setPage(page);
+                        setUsers([...users, ...items]);
+                        setLoaded(true);
+                        setLastPage(lastPage);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+            return () => {
+                mounted = false;
+                abortController.abort();
+            };
+        }
+    }, [loaded]);
+
+    const renderItem = useCallback(({ item }: { item: UserSearch }) => {
+        return (
+            <UserListItem
+                firstname = {item.firstname}
+                id = {item.id}
+                activityArea = {item.activityArea}
+            />
+        )
+    }, []);
+
+    const onEndReached = useCallback(() => {
+        if (!lastPage) {
+            setLoaded(false);
+        }
+    }, [lastPage]);
+
     return (
         <Layout style={{ flex: 1 }} level='1'>
-            <Text>Recherche</Text>
+            {
+                !loaded && (<SpinnerList />)
+            }
+            <List
+                data={users}
+                renderItem={renderItem}
+                keyExtractor={(item:any) => item.id}
+                onEndReachedThreshold={0.5}
+                onEndReached={onEndReached}
+            />
         </Layout>
     );
 }
