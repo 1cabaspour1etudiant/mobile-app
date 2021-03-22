740aecf4af9caee53111b5aa5026f6b34f6adda7
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-22 21:40:12 +0100

    feat: import SearchUserListItem from an other file

---
+++ src/screens/Private/SearchTab/SearchTab.tsx
@@ -0,0 +1,70 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { Layout, List } from '@ui-kitten/components';
+import { getUserSearch } from '../../../api/User';
+import { UserSearch } from '../../../api/types';
+import SpinnerList from '../../../components/SpinnerList';
+import SearchUserListItem from './SearchUserListItem';
+
+export default function SearchTab() {
+    const [loaded, setLoaded] = useState(false);
+    const [users, setUsers] = useState<UserSearch[]>([]);
+    const [page, setPage] = useState(-1);
+    const [lastPage, setLastPage] = useState(false);
+
+    useEffect(() => {
+        if (!loaded) {
+            let mounted = true;
+            const abortController = new AbortController();
+
+            getUserSearch(page + 1, 20, abortController)
+                .then(({ page, items, lastPage }) => {
+                    if (mounted) {
+                        setPage(page);
+                        setUsers([...users, ...items]);
+                        setLoaded(true);
+                        setLastPage(lastPage);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+            return () => {
+                mounted = false;
+                abortController.abort();
+            };
+        }
+    }, [loaded]);
+
+    const renderItem = useCallback(({ item }: { item: UserSearch }) => {
+        return (
+            <SearchUserListItem
+                id = {item.id}
+                firstname={item.firstname}
+                activityArea = {item.activityArea}
+            />
+        )
+    }, []);
+
+    const onEndReached = useCallback(() => {
+        if (!lastPage) {
+            setLoaded(false);
+        }
+    }, [lastPage]);
+
+    return (
+        <Layout style={{ flex: 1 }} level='1'>
+            {
+                !loaded && (<SpinnerList />)
+            }
+            <List
+                data={users}
+                renderItem={renderItem}
+                keyExtractor={(item:any) => item.id}
+                onEndReachedThreshold={0.5}
+                onEndReached={onEndReached}
+            />
+        </Layout>
+    );
+}
---
+++ src/screens/Private/SearchTab/SearchUserListItem.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Image } from 'react-native';
+import { Button, Icon, ListItem } from '@ui-kitten/components';
+import { getUserProfilePicture} from '../../../api/User';
+import { UserSearch } from '../../../api/types';
+
+export default function SearchUserListItem({ id, firstname, activityArea }: UserSearch) {
+    const [ pictureLoaded, setPictureLoaded ] = useState(false);
+    const [picture, setPicture] = useState('');
+
+    useEffect(() => {
+        if (!pictureLoaded) {
+            let mounted = true;
+            const abortController = new AbortController();
+
+            getUserProfilePicture(id, abortController)
+                .then((pictureAsBase64) => {
+                    if (mounted) {
+                        setPictureLoaded(true);
+                        setPicture(pictureAsBase64);
+                    }
+                })
+                .catch((error) => {
+                    // console.log(error);
+                });
+
+            return () => {
+                mounted = false;
+                abortController.abort();
+            };
+        }
+    }, [pictureLoaded]);
+
+    const renderItemAccessoryRight = useCallback(() => {
+        return (
+            <Button size='tiny'>Contacte</Button>
+        );
+    }, []);
+
+    const renderItemAccessoryLeft = useCallback((props) => {
+        if (pictureLoaded) {
+            return (
+                <Image source={{ uri: picture }}/>
+            );
+        }
+
+        return (
+            <Icon {...props} name='person'/>
+        );
+    }, [pictureLoaded, picture]);
+
+    return (
+        <ListItem
+            title={firstname}
+            description={activityArea}
+            accessoryLeft={renderItemAccessoryLeft}
+            accessoryRight={renderItemAccessoryRight}
+        />
+    );
+}
