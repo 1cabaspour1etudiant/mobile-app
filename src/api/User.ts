db00c7ebc46cc51205b931fb99983c6e19ef632e
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-24 17:37:43 +0100

    feat: create api caller GetUserInfos

--- src/api/User.ts
+++ src/api/User.ts
@@ -3,7 +3,7 @@ import { UserRegister } from '../screens/Register/types';
 import { checkError, getAccessToken, jsonMapper } from './utils';
 import Store from '../../store';
 import { actionSetToken } from '../screens/token.action';
-import { AwaitingSponsorshipResponse, UserSearchResponse } from './types';
+import { AwaitingSponsorshipResponse, GetUserInfos, UserSearchResponse } from './types';
 import { PrivateUserInfos } from '../screens/Private/types';
 
 export function getUserEmailIsAvailable(email: string): Promise<boolean> {
@@ -164,4 +164,23 @@ export async function getSponsorshipRequests(
     return fetch(url, options)
         .then(checkError)
         .then(jsonMapper);
+}
+
+export async function getUserInfos(userId: number, abortController = new AbortController()):Promise<GetUserInfos> {
+    const accessToken = await getAccessToken();
+    const params = new URLSearchParams({ userId: userId.toString() });
+    const url = `${API_URL}/user?${params}`;
+    const headers = new Headers();
+    headers.append('Authorization', `Bearer ${accessToken}`);
+
+    const { signal } = abortController;
+    const options = {
+        method: 'GET',
+        headers,
+        signal,
+    };
+
+    return fetch(url, options)
+        .then(checkError)
+        .then(jsonMapper);    
 }
