7b60bf3347ee8183c3fbc412ae0859805127792c
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-24 15:10:21 +0100

    feat create action for set user's profile's picture

--- src/screens/Private/user.action.ts
+++ src/screens/Private/user.action.ts
@@ -1,10 +1,18 @@
 import { PrivateUserInfos } from "./types";
 
 export const PRIVATE_USER_SET_INFOS = 'PRIVATE_USER_SET_INFOS';
+export const PRIVATE_USER_SET_PROFILE_PICTURE = 'PRIVATE_USER_SET_PROFILE_PICTURE';
 
 export function actionPrivateUserSetInfos(infos: PrivateUserInfos) {
     return {
         type: PRIVATE_USER_SET_INFOS,
         value: infos,
+    };
+}
+
+export function actionPrivateUserSetProfilePicture(profilePicture: string) {
+    return {
+        type: PRIVATE_USER_SET_PROFILE_PICTURE,
+        value: profilePicture,
     };
 }
