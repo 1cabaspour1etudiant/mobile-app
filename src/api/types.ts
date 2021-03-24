cf254962992e6fa9de926933304a830e8eebdc8c
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-24 15:55:32 +0100

    fix: use proper type for items of sponsorship request

--- src/api/types.ts
+++ src/api/types.ts
@@ -24,6 +24,6 @@ export type Sponsorship = {
 export type AwaitingSponsorshipResponse = {
     page: number;
     lastPage: boolean;
-    items: UserSearch[];
+    items: Sponsorship[];
     pageSize: number;
 }
