6a8ae9e4c6211e75bc8cd2923968eb67e12bbec6
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-24 15:34:33 +0100

    fix: remove usless performance properties

--- src/screens/Private/SearchTab/SearchTab.tsx
+++ src/screens/Private/SearchTab/SearchTab.tsx
@@ -65,12 +65,7 @@ export default function SearchTab() {
                 keyExtractor={(item:UserSearch) => `${item.id}`}
                 onEndReachedThreshold={0.25}
                 onEndReached={onEndReached}
-
-                removeClippedSubviews={true} // Unmount components when outside of window 
-                initialNumToRender={2} // Reduce initial render amount
-                maxToRenderPerBatch={1} // Reduce number in each render batch
-                updateCellsBatchingPeriod={100} // Increase time between renders
-                windowSize={7} // Reduce the window size
+                removeClippedSubviews={true}
             />
         </Layout>
     );
