07bb2ee4c4dc1be4279e09f579c2d67e9ec5b345
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-24 21:56:11 +0100

    feat: create RequestsTab

--- src/screens/Private/RequestsTab/RequestsTab.tsx
+++ src/screens/Private/RequestsTab/RequestsTab.tsx
@@ -1,11 +1,75 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Layout } from '@ui-kitten/components';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Layout, List } from '@ui-kitten/components';
+import { Sponsorship } from '../../../api/types';
+import { getSponsorshipRequests } from '../../../api/Sponsorship';
+import SpinnerList from '../../../components/SpinnerList';
+import ReceivedRequestSponsorshipListItem from './ReceivedRequestSponsorshipListItem';
 
 export default function RequestsTab() {
+    const [loaded, setLoaded] = useState(false);
+    const [sponsorships, setSponsorships] = useState<Sponsorship[]>([]);
+    const [page, setPage] = useState(-1);
+    const [lastPage, setLastPage] = useState(false);
+
+    useEffect(() => {
+        if (!loaded) {
+            let mounted = true;
+            const abortController = new AbortController();
+
+            getSponsorshipRequests(page + 1, 20, 'received', abortController)
+                .then(({ page, items, lastPage }) => {
+                    if (mounted) {
+                        setPage(page);
+                        setSponsorships([...sponsorships, ...items]);
+                        setLoaded(true);
+                        setLastPage(lastPage);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+            return () => {
+                mounted = false;
+                abortController.abort();
+            };
+        }
+    }, [loaded]);
+
+    const renderItem = useCallback(({ item }: { item: Sponsorship }) => {
+        return (
+            <ReceivedRequestSponsorshipListItem
+                key={item.sponsorshipId}
+                date={item.date}
+                emitterId={item.emitterId}
+                recipientId={item.recipientId}
+                godsonId={item.godsonId}
+                godfatherId={item.godfatherId}
+                sponsorshipId={item.sponsorshipId}
+                validated={item.validated}
+            />
+        );
+    }, []);
+
+    const onEndReached = useCallback(() => {
+        if (!lastPage) {
+            setLoaded(false);
+        }
+    }, [lastPage]);
+
     return (
         <Layout style={{ flex: 1 }} level='1'>
-            <Text>Requests</Text>
+            {
+                !loaded && (<SpinnerList />)
+            }
+            <List
+                data={sponsorships}
+                renderItem={renderItem}
+                keyExtractor={(item:Sponsorship) => `${item.sponsorshipId}`}
+                onEndReachedThreshold={0.25}
+                onEndReached={onEndReached}
+                removeClippedSubviews={true}
+            />
         </Layout>
     );
 }
