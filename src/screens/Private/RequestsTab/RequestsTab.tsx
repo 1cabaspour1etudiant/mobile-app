ad3d7556a36de5c4da82033265f43a9cd21da338
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-25 21:31:37 +0100

    feat: add refreshControl on request list

--- src/screens/Private/RequestsTab/RequestsTab.tsx
+++ src/screens/Private/RequestsTab/RequestsTab.tsx
@@ -1,13 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Layout, List, Text } from '@ui-kitten/components';
-import { useSelector } from 'react-redux';
-
 import { Sponsorship } from '../../../api/types';
 import { getSponsorshipRequests } from '../../../api/Sponsorship';
 import SpinnerList from '../../../components/SpinnerList';
 import ReceivedRequestSponsorshipListItem from './ReceivedRequestSponsorshipListItem';
 import { useUserStatus } from '../../hooks';
-
+import { RefreshControl } from 'react-native';
+import { default as theme } from '../../../../theme.json';
 
 
 export default function RequestsTab() {
@@ -15,7 +14,7 @@ export default function RequestsTab() {
     const [sponsorships, setSponsorships] = useState<Sponsorship[]>([]);
     const [page, setPage] = useState(-1);
     const [lastPage, setLastPage] = useState(false);
-
+    const [refreshing, setRefreshing] = useState(false);
     const status = useUserStatus();
 
     useEffect(() => {
@@ -43,6 +42,35 @@ export default function RequestsTab() {
         }
     }, [loaded]);
 
+    useEffect(() => {
+        if (refreshing) {
+            let mounted = true;
+            (async () => {
+                const sponsorships = [];
+                for (let i = 0; i < page; i++) {
+                    const { items } = await getSponsorshipRequests(i, 20, 'received');
+                    sponsorships.push(...items);
+                }
+
+                if (mounted) {
+                    setSponsorships(sponsorships);
+                }
+            })()
+                .catch((error) => {
+                    console.log(error);
+                })
+                .finally(() => {
+                    if (mounted) {
+                        setRefreshing(false);
+                    }
+                });
+
+            return () => {
+                mounted = false;
+            };
+        }
+    }, [refreshing]);
+
     const renderItem = useCallback(({ item }: { item: Sponsorship }) => {
         return (
             <ReceivedRequestSponsorshipListItem
@@ -64,12 +92,27 @@ export default function RequestsTab() {
         }
     }, [lastPage]);
 
+    const onRefresh = useCallback(() => {
+        if (!refreshing) {
+            setRefreshing(true);
+        }
+    }, [refreshing]);
+
     return (
         <Layout style={{ flex: 1 }} level='1'>
             {
                 !loaded && (<SpinnerList />)
             }
-            <Text category='h6'>Vos { status === 'godfather' ? 'demandes' : 'propositions' } de sponsorisation</Text>
+            {
+                loaded && sponsorships.length !== 0 && (
+                    <Text category='h6'>Vos { status === 'godfather' ? 'demandes' : 'propositions' } de sponsorisation</Text>
+                )
+            }
+            {
+                loaded && sponsorships.length === 0 && (
+                    <Text category='h6' style={{ textAlign: 'center' }}>Vous n'avez pas de demandes de en attentes</Text>
+                )
+            }
             <List
                 data={sponsorships}
                 renderItem={renderItem}
@@ -77,6 +120,13 @@ export default function RequestsTab() {
                 onEndReachedThreshold={0.25}
                 onEndReached={onEndReached}
                 removeClippedSubviews={true}
+                refreshControl={
+                    <RefreshControl
+                        colors={[theme['color-primary-700']]}
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
             />
         </Layout>
     );
