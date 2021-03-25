d4d7009408c4a572d831c54712816513c9dd3fa6
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-25 21:55:41 +0100

    feat: show corrresponding tab with user's status

--- src/screens/Private/MemberSpace.tsx
+++ src/screens/Private/MemberSpace.tsx
@@ -6,9 +6,10 @@ import {
 import { BottomTabBarOptions, BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaView } from 'react-native';
 
-import { useLeaveApp } from '../hooks';
+import { useLeaveApp, useUserStatus } from '../hooks';
 import SearchTab from './SearchTab/SearchTab';
 import GodefatherTab from './GodfatherTab/GodefatherTab';
+import GodsonTab from './GodsonTab/GodsonTab';
 import ProfileTab from './ProfileTab/ProfileTab';
 import RequestsTab from './RequestsTab/RequestsTab';
 
@@ -19,13 +20,14 @@ function BottomTabBar({ navigation, state}: BottomTabBarProps<BottomTabBarOption
         (index) => navigation.navigate(state.routeNames[index]),
     []);
 
+    const status = useUserStatus();
     return (
         <BottomNavigation
             selectedIndex={state.index}
             onSelect={onSelect}
         >
             <BottomNavigationTab title='Recherche' />
-            <BottomNavigationTab title='Parrain'/>
+            <BottomNavigationTab title={status === 'godson' ? 'Parrain' : 'Filleuls' }/>
             <BottomNavigationTab title='Demandes'/>
             <BottomNavigationTab title='Profil'/>
         </BottomNavigation>
@@ -34,11 +36,20 @@ function BottomTabBar({ navigation, state}: BottomTabBarProps<BottomTabBarOption
 
 export default function MemberSpace() {
     useLeaveApp();
+    const status = useUserStatus();
+    let relationScreen;
+
+    if (status === 'godson') {
+        relationScreen = <Screen name='Parrain' component={GodefatherTab}/>
+    } else {
+        relationScreen = <Screen name='Filleuls' component={GodsonTab}/>
+    }
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Navigator tabBar={props => <BottomTabBar {...props} />}>
             <Screen name='Recherche' component={SearchTab}/>
-            <Screen name='Parrain' component={GodefatherTab}/>
+            {relationScreen}
             <Screen name='Demandes' component={RequestsTab}/>
             <Screen name='Profil' component={ProfileTab}/>
             </Navigator>
