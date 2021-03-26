3ebc5da7e8ab308a7be64ae6bf6ea4e97ff48804
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-26 16:38:33 +0100

    feat: create empty drawer

--- src/screens/Private/MemberSpace.tsx
+++ src/screens/Private/MemberSpace.tsx
@@ -5,6 +5,7 @@ import {
 } from '@ui-kitten/components';
 import { BottomTabBarOptions, BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaView } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { useLeaveApp, useUserStatus } from '../hooks';
 import SearchTab from './SearchTab/SearchTab';
@@ -12,7 +13,9 @@ import GodefatherTab from './GodfatherTab/GodefatherTab';
 import GodsonTab from './GodsonTab/GodsonTab';
 import ProfileTab from './ProfileTab/ProfileTab';
 import RequestsTab from './RequestsTab/RequestsTab';
+import {default as theme} from '../../../theme.json';
 
+const Drawer = createDrawerNavigator();
 const { Navigator, Screen } = createBottomTabNavigator();
 
 function BottomTabBar({ navigation, state}: BottomTabBarProps<BottomTabBarOptions>) {
@@ -34,7 +37,7 @@ function BottomTabBar({ navigation, state}: BottomTabBarProps<BottomTabBarOption
     );
 }
 
-export default function MemberSpace() {
+function TabBarContainer() {
     useLeaveApp();
     const status = useUserStatus();
     let relationScreen;
@@ -46,13 +49,32 @@ export default function MemberSpace() {
     }
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Navigator tabBar={props => <BottomTabBar {...props} />}>
+        <Navigator tabBar={props => <BottomTabBar {...props} />}>
             <Screen name='Recherche' component={SearchTab}/>
             {relationScreen}
             <Screen name='Demandes' component={RequestsTab}/>
             <Screen name='Profil' component={ProfileTab}/>
-            </Navigator>
+        </Navigator>
+    );
+}
+
+function DrawerContent() {
+    return (
+        <></>
+    );
+}
+
+export default function MemberSpace() {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Drawer.Navigator
+                initialRouteName="TabBarContainer"
+                overlayColor={theme['color-primary-transparent-600']}
+                edgeWidth={25}
+                drawerContent={(props:any) => <DrawerContent {...props} />}
+            >
+                <Drawer.Screen name="TabBarContainer" component={TabBarContainer} />
+            </Drawer.Navigator>
         </SafeAreaView>
     );
 }
