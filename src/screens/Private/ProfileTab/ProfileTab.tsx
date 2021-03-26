73651daca71e12ea03c34093b3299a210813bc99
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-26 16:49:34 +0100

    feat: create empty profile tab

--- src/screens/Private/ProfileTab/ProfileTab.tsx
+++ src/screens/Private/ProfileTab/ProfileTab.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { Layout } from '@ui-kitten/components';
+import { useUserInfos } from '../../hooks';
 
 export default function ProfileTab() {
+    const userInfos = useUserInfos();
     return (
         <Layout style={{ flex: 1 }} level='1'>
-            <Text>Profil</Text>
+            <Text>{userInfos.firstname}</Text>
+            <Text>{userInfos.lastname}</Text>
         </Layout>
     );
 }
