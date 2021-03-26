b0137fad678d07ae58dd81a854cc1cc32c2fe86d
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-26 17:35:47 +0100

    feat: drawer add identity and picturProfile

--- src/screens/Private/DrawerContent/DrawerContent.tsx
+++ src/screens/Private/DrawerContent/DrawerContent.tsx
@@ -1,15 +1,20 @@
 import React, { useCallback } from 'react';
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 
-import { Layout, Button } from '@ui-kitten/components';
+import { Layout, Button, Text, Icon } from '@ui-kitten/components';
 import { useNavigation } from '@react-navigation/core';
 import { useDispatch } from 'react-redux';
 import { actionLogOut } from '../../token.action';
+import { useUserInfos, useUserProfilePicture } from '../../hooks';
+import {default as theme} from '../../../../theme.json';
 
 export default function DrawerContent() {
     const { navigate } = useNavigation();
     const dispatch = useDispatch();
+    const userInfos = useUserInfos();
+    const profilePicture = useUserProfilePicture();
+
     const toggleOnPressLogOutButton = useCallback(() => {
         dispatch(actionLogOut());
         navigate('LoginScreen');
@@ -17,14 +22,36 @@ export default function DrawerContent() {
 
     return (
         <Layout style={styles.container} level='1'>
-            <Button
-                style={styles.logOutButton}
-                appearance='outline'
-                status='primary'
-                onPress={toggleOnPressLogOutButton}
-            >
-                Se deconnecter
-            </Button>
+            <View style={styles.topSection}>
+                {
+                    profilePicture === '' ? (
+                        <Icon
+                            fill={theme['color-primary-700']}
+                            name='person-outline'
+                            style={styles.pictureProfile}
+                        />
+                    ) : (
+                        <Image
+                            source={{ uri:`data:;base64, ${profilePicture}` }}
+                            style={styles.pictureProfile}
+                        />
+                    )
+                }
+                <View style={styles.fullnameContainer}>
+                    <Text>{userInfos.firstname}</Text>
+                    <Text>{userInfos.lastname}</Text>
+                </View>
+            </View>
+            <View style={styles.bottomSection}>
+                <Button
+                    style={styles.logOutButton}
+                    appearance='outline'
+                    status='primary'
+                    onPress={toggleOnPressLogOutButton}
+                >
+                    Se deconnecter
+                </Button>
+            </View>
         </Layout>
     );
 }
@@ -34,6 +61,25 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingLeft: 15,
         paddingRight: 15,
+        paddingTop: 15,
+    },
+    topSection: {
+        flex: 3,
+        alignItems: 'center'
+    },
+    pictureProfile: {
+        width: 150,
+        height: 150,
+        borderRadius: 100,
+    },
+    fullnameContainer: {
+        marginTop: 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 90,
+    },
+    bottomSection: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'flex-end',
     },
