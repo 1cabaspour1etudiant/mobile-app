865cd849db36297f5cc1b18ecb0b336b88d3f6a6
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-29 15:59:52 +0200

    feat: print message for godson which already has a godfather

--- src/screens/Private/SearchTab/SearchTab.tsx
+++ src/screens/Private/SearchTab/SearchTab.tsx
@@ -1,16 +1,20 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
-import { Layout, List } from '@ui-kitten/components';
+import { Layout, List, Text } from '@ui-kitten/components';
+import { StyleSheet, View } from 'react-native';
 import { getUserSearch } from '../../../api/User';
 import { UserSearch } from '../../../api/types';
 import SpinnerList from '../../../components/SpinnerList';
 import SearchUserListItem from './SearchUserListItem';
+import { useUserHasGodfather } from '../../hooks';
 
 export default function SearchTab() {
     const [loaded, setLoaded] = useState(false);
     const [users, setUsers] = useState<UserSearch[]>([]);
     const [page, setPage] = useState(-1);
     const [lastPage, setLastPage] = useState(false);
+
+    const hasGodfather = useUserHasGodfather();
 
     useEffect(() => {
         if (!loaded) {
@@ -58,16 +62,37 @@ export default function SearchTab() {
     return (
         <Layout style={{ flex: 1 }} level='1'>
             {
-                !loaded && (<SpinnerList />)
+                hasGodfather ? (
+                    <View style={styles.sentenceContainer}>
+                        <Text category='h4'>Vous possedez déjà un parrain</Text>
+                    </View>
+                ) : (
+                    <>
+                        {
+                            !loaded && (<SpinnerList />)
+                        }
+                        <List
+                            data={users}
+                            renderItem={renderItem}
+                            keyExtractor={(item:UserSearch) => `${item.id}`}
+                            onEndReachedThreshold={0.25}
+                            onEndReached={onEndReached}
+                            removeClippedSubviews={true}
+                        />
+                    </>
+                )
             }
-            <List
-                data={users}
-                renderItem={renderItem}
-                keyExtractor={(item:UserSearch) => `${item.id}`}
-                onEndReachedThreshold={0.25}
-                onEndReached={onEndReached}
-                removeClippedSubviews={true}
-            />
         </Layout>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    sentenceContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
