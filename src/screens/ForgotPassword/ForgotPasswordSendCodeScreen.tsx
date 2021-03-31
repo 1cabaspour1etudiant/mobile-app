2dacace758b4758d857eb5a0428d5901d3de11cb
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-31 15:44:30 +0200

    feat: create screen ForgotPasswordSendCode

--- src/screens/ForgotPassword/ForgotPasswordSendCodeScreen.tsx
+++ src/screens/ForgotPassword/ForgotPasswordSendCodeScreen.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
+import { Layout, Text, Input, Button, } from '@ui-kitten/components';
+import { StyleSheet, View } from 'react-native';
 
 export default function ForgotPasswordSendCodeScreen() {
+
     return (
         <Layout style={styles.container}>
-            <Text category='h5'>Mot de passe oublié: envoi de code</Text>
+            <View style={styles.wrapper}>
+                <Text category='h5' style={styles.title}>Réinitialiser votre mot de passe</Text>
+                <Text category='h6' style={styles.subTitle}>Recevoir un code de reinitialisation</Text>
+                <Input
+                    style={styles.input}
+                    placeholder='Email'
+                    keyboardType='email-address'
+                />
+
+                <Button
+                    style={styles.input}
+                >
+                    Envoyer
+                </Button>
+            </View>
         </Layout>
     )
 }
@@ -14,6 +29,23 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    wrapper: {
+        height: '50%',
+    },
+    title: {
+
+    },
+    subTitle: {
+        marginTop: 15,
+    },
+    input: {
+        marginTop: 15,
+    },
+    button: {
+        marginTop: 15,
     },
 });
