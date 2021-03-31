169b24ed2a012124da04e9a492525a6b50d82fae
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-31 16:05:17 +0200

    feat: create actions for set verification code and new password

--- src/screens/ForgotPassword/action.ts
+++ src/screens/ForgotPassword/action.ts
@@ -1,8 +1,24 @@
 export const FORGOT_PASSWORD_SET_EMAIL = 'FORGOT_PASSWORD_SET_EMAIL';
+export const FORGOT_PASSWORD_SET_VERIFICATION_CODE = 'FORGOT_PASSWORD_SET_VERIFICATION_CODE';
+export const FORGOT_PASSWORD_SET_NEW_PASSWORD = 'FORGOT_PASSWORD_SET_NEW_PASSWORD';
 
 export function actionForgotPasswordSetEmail(email: string) {
     return {
         type: FORGOT_PASSWORD_SET_EMAIL,
         value: email,
+    };
+}
+
+export function actionForgotPasswordSetVerificationCode(code: string) {
+    return {
+        type: FORGOT_PASSWORD_SET_VERIFICATION_CODE,
+        value: code,
+    };
+}
+
+export function actionForgotPasswordSetNewPassword(password: string) {
+    return {
+        type: FORGOT_PASSWORD_SET_NEW_PASSWORD,
+        value: password,
     };
 }
