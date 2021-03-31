60fee2fe17eb16227251baceedc0af8f14da39e8
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-31 21:51:34 +0200

    feat: handle writting of code and paswword properties

--- src/screens/ForgotPassword/reducer.ts
+++ src/screens/ForgotPassword/reducer.ts
@@ -1,5 +1,8 @@
 import { ForgottenPasswordState } from '../../types';
-import { FORGOT_PASSWORD_SET_EMAIL } from './action';
+import { FORGOT_PASSWORD_SET_EMAIL,
+    FORGOT_PASSWORD_SET_NEW_PASSWORD,
+    FORGOT_PASSWORD_SET_VERIFICATION_CODE
+} from './action';
 
 const initialState: ForgottenPasswordState = {
     email: '',
@@ -12,6 +15,12 @@ export default function(state = initialState, action: any) {
     switch(action.type) {
         case FORGOT_PASSWORD_SET_EMAIL:
             return {...state, email: action.value};
+
+        case FORGOT_PASSWORD_SET_VERIFICATION_CODE:
+            return {...state, code: action.value};
+
+        case FORGOT_PASSWORD_SET_NEW_PASSWORD:
+            return {...state, password: action.value};
 
         default:
             return state;
