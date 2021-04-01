2c02d18aeec0023b5ca7be278b913b0d9cefe0c4
kecsou <kevin.voyer.developpeur@gmail.com>
2021-04-01 15:51:21 +0200

    feat: create action for refreshing search tab

--- src/screens/Private/user.action.ts
+++ src/screens/Private/user.action.ts
@@ -4,6 +4,8 @@ export const PRIVATE_USER_SET_INFOS = 'PRIVATE_USER_SET_INFOS';
 export const PRIVATE_USER_SET_PROFILE_PICTURE = 'PRIVATE_USER_SET_PROFILE_PICTURE';
 export const PRIVATE_USER_SET_HAS_GODFATHER = 'PRIVATE_USER_SET_HAS_GODFATHER';
 
+export const PRIVATE_USER_REFRESH_SEARCH_TAB = 'PRIVATE_USER_REFRESH_SEARCH_TAB';
+
 export function actionPrivateUserSetInfos(infos: UserInfos) {
     return {
         type: PRIVATE_USER_SET_INFOS,
@@ -22,5 +24,11 @@ export function actionPrivateUserSetHasGodfather(hasGodfather: boolean) {
     return {
         type: PRIVATE_USER_SET_HAS_GODFATHER,
         value: hasGodfather,
+    };
+}
+
+export function actionPrivateUserRefreshSearchTab() {
+    return {
+        type: PRIVATE_USER_REFRESH_SEARCH_TAB,
     };
 }
