299942efaba18f9543f2f135ccdfec82e0c65ed8
kecsou <kevin.voyer.developpeur@gmail.com>
2021-04-01 14:04:09 +0200

    feat: add notification subscription

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 
+import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {
   createStackNavigator,
@@ -9,6 +10,8 @@ import {
 
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+
+import * as Notifications from 'expo-notifications';
 
 import LoginScreen from './src/screens/Login/Login';
 import MemberSpaceScreen from './src/screens/Private/MemberSpace';
@@ -27,8 +30,47 @@ import { default as theme } from './theme.json';
 
 const Stack = createStackNavigator();
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldSetBadge: false,
+    shouldPlaySound: true,
+  }),
+});
+
+if (Platform.OS === 'android') {
+  Notifications.setNotificationChannelAsync('default', {
+    name: 'default',
+    importance: Notifications.AndroidImportance.MAX,
+    vibrationPattern: [0, 250, 250, 250],
+    lightColor: theme['color-primary-100'],
+  });
+}
+
+function handleNotification(notification: Notifications.Notification, tap?:boolean) {
+
+}
+
 export default function App() {
   const [loaded, setLoaded]  = useState(false);
+
+  useEffect(() => {
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    const notificationListener = Notifications.addNotificationReceivedListener((notification) => {
+      handleNotification(notification, false);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification
+    // (works when app is foregrounded, backgrounded, or killed)
+    const responseListener = Notifications.addNotificationResponseReceivedListener((response) => {
+      handleNotification(response.notification, true);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
 
   useEffect(() => {
     if (!loaded) {
